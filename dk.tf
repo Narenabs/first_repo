4345987d1c32a54d89c433bba5e49b972931ff89
naren m <narendhiran1423@gmail.com>
2022-12-09 00:40:32 +0530

    hi

---
+++ dk.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIARIQWLXYMVKQU2ZUZ"
+  secret_key = "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"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "abs"
+  }
+}
